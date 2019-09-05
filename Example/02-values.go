a115b8cdd79213087a03ae27c5f79e2ba354ef2e
sugoi <promacanthus@qq.com>
2019-09-05 13:52:24 +0800

    init commit

---
+++ Example/02-values.go
@@ -0,0 +1,18 @@
+package Example
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+
+	fmt.Println(true&&false)
+	fmt.Println(true||false)
+	fmt.Println(!true)
+}
+
+// 使用加号（+）可以将string合并到一起
+
+// 布尔运算符是短路运算符
---
+++ Example/03-variables.go
@@ -0,0 +1,28 @@
+package Example
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Print(f)
+}
+
+// var 声明一个或多个变量，可以在一次声明中同时声明多个变量
+
+// Go将会推断初始化变量的类型
+
+// 在声明变量时没有对它进行任何初始化操作，那么该变量的默认值为该类型的零值。（如 int 的零值为 0）
+
+// := 句法是一种短变量声明和初始化的方式。（如 上面的变量f的声明和初始化）
